201e7591c066ed0b168c17bf5dcda7b554fefd5a
PraveeDurairaj <duraipraveen8834@gmail.com>
2025-05-10 10:59:43 -0700

    projects image section addtion

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
     <div className='container'>
         <h2 className='common_h2 mb-20 text-center'>Projects</h2>
         <div className="projects_container">
-        <Image src={gymWebsite} width={400} height={300} alt={'projects'}/>
-        <Image src={portfolio} width={400} height={300} alt={'projects'}/>
+        <Image src={gymWebsite} alt={'projects'}/>
+        <Image src={portfolio} alt={'projects'}/>
         </div>
         
     </div>
